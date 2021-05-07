8173d7197eed4e1897590a27b1d6bea5528ab3e1
Adraah <80177793+Adraah@users.noreply.github.com>
2021-05-07 16:53:09 -0500

    Add files via upload

---
+++ GeneradorCurp.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Core
+{
+    public static class GeneradorCurp
+    {
+        public static string Generar(string nombre, string apellidoP, string apellidoM, string fecha, string estado,string sexo)
+        {
+            nombre = nombre.ToUpperInvariant();
+            apellidoP = apellidoP.ToUpperInvariant();
+            apellidoM = apellidoM.ToUpperInvariant();
+            estado = estado.ToUpperInvariant();
+
+            var curp = new StringBuilder();
+
+            curp.Append(apellidoP.Substring(0,2));
+            curp.Append(apellidoM.Substring(0, 1));
+            curp.Append(nombre.Substring(0, 1));
+            curp.Append(fecha);
+            curp.Append(sexo);
+            curp.Append(estado);
+            curp.Append(apellidoP.Substring(1, 2));
+            curp.Append(apellidoM.Substring(1, 2));
+            curp.Append(nombre.Substring(1, 2));
+            curp.Append("0");
+            curp.Append("9");
+
+            return curp.ToString();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Act14
+{
+    public partial class MainPage : ContentPage
+    {
+        string curp;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnAceptar(object sender, EventArgs e)
+        {
+            curp = Core.GeneradorCurp.Generar(Nombre.Text, ApellidoP.Text, ApellidoM.Text,Fecha.Text,Estado.Text,Sexo.Text);
+            if (!string.IsNullOrWhiteSpace(curp))
+            {
+                Curp.Text = curp;
+            }
+            else
+            {
+                Curp.Text = " ";
+            }
+        }
+    }
+}
